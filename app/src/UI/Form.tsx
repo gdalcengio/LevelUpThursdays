411e4ebffa1c5d779dd1369bb7d6e4b12509af07
Micheal Wells <micheal.w.wells@gmail.com>
2023-12-07 10:19:12 -0800

    more css + divs

--- app/src/UI/Form.tsx
+++ app/src/UI/Form.tsx
@@ -1,18 +1,17 @@
 import { useRef } from "react";
-import "./Form.css"
-
+import "./Form.css";
 
 export const FormPanel = (props: any) => {
   const ref = useRef(0);
   ref.current += 1;
   console.log("%FormPanel render:" + ref.current.toString(), "color: yellow");
 
-    return <div className="FormPanel">
-
-
-            <div className="inputsContainer"></div>
-
-
-
+  return (
+    <div className="FormPanel">
+      <div className="inputsContainer">
+        <div className="headerBar" />
+        <div className="meese" />
+      </div>
     </div>
-}
+  );
+};
