5766000546727b11be360214e1e6a6979dcab50a
lunamoonmoon <5862lunamoon@gmail.com>
2023-12-07 14:07:13 -0800

    added initial map

--- app/src/UI/Map.tsx
+++ app/src/UI/Map.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import "./Map.css"
-
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 
 export const MapPanel = (props: any) => {
@@ -8,5 +8,12 @@ export const MapPanel = (props: any) => {
   ref.current += 1;
   console.log("%MapPanel render:" + ref.current.toString(), "color: yellow");
 
-    return <div className="MapPanel"></div>
+  return <div className="MapPanel">
+      <MapContainer className="MapContainer" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+  </div>
 }
