eac09f1c75d3fa665414796ea6ff7675d2e6c02d
Micheal Wells <micheal.w.wells@gmail.com>
2023-12-14 11:53:30 -0800

    missing stuff

--- app/src/UI/Regs.tsx
+++ app/src/UI/Regs.tsx
@@ -1,18 +1,16 @@
 import { useEffect, useRef } from "react";
 import "./Regs.css";
 
-import { Browser } from '@capacitor/browser';
+import { Browser } from "@capacitor/browser";
+import { PermissionState } from "@capacitor/core";
 
 export const Regs = (props: any) => {
   const ref = useRef(0);
   ref.current += 1;
   console.log("%Regs render:" + ref.current.toString(), "color: yellow");
-  
-  useEffect(() => {
-    Browser.open({ url: 'http://www.africau.edu/images/default/sample.pdf'});
-  }, []);
-  return (
-    <div className="PDFPanel">
-    </div>
-  );
+
+  const onClick = () => {
+    Browser.open({ url: "http://www.africau.edu/images/default/sample.pdf" });
+  };
+  return <div className="PDFPanel" onClick={onClick}></div>;
 };
