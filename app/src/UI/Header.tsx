b9040957e0597c52147eda6f16df20f0c8869843
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-12-14 14:29:28 -0800

    Add about page with some temporary hipster ipsum

--- app/src/UI/Header.tsx
+++ app/src/UI/Header.tsx
@@ -1,14 +1,29 @@
 import "./Header.css"
+import { useNavigate } from "react-router-dom";
 
 export const Header = (props: any) => {
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate('/About');
+  }
+
+  const goToForm = () => {
+    navigate('/');
+  }
 
     return(
         <div className="headerWrapper">
             <div className="iconContainer">
                 <img className="bcgovIcon" src="bcgov_logo.svg" alt="Government of British Columbia" />
             </div>
-            <div className="titleContainer">
+            <div className="titleContainer" onClick={goToForm}>
                 <p className="headerText">Moose Tracker</p>
+            </div>
+            <div className="about-button-container">
+                <button className="about-button" onClick={goHome}>
+                  About
+                </button>
             </div>
         </div>
     )
