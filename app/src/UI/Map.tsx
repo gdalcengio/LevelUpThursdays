671eeb5927cf95194ce965175c8a7f7e390e7fe8
Micheal Wells <micheal.w.wells@gmail.com>
2023-12-14 15:31:26 -0800

    change offset to be based on pixels

--- app/src/UI/Map.tsx
+++ app/src/UI/Map.tsx
@@ -1,103 +1,132 @@
-import "./Map.css"
-import { MapContainer, Marker, TileLayer, useMap } from 'react-leaflet'
-import { LatLngExpression, Icon } from 'leaflet'
+import "./Map.css";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { LatLngExpression, Icon } from "leaflet";
 import { useSelector } from "react-redux";
+import L from "leaflet";
+import React from "react";
 
 interface ChangeViewProps {
-    center: LatLngExpression;
+  center: LatLngExpression;
 }
 
+const mooseIconMale = new Icon({
+  iconUrl: "moose.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const mooseIconFemale = new Icon({
+  iconUrl: "fmoose.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const mooseIconCalf = new Icon({
+  iconUrl: "calf.png",
+  iconSize: [30, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 const ChangeView: React.FC<ChangeViewProps> = ({ center }) => {
-    const map = useMap();
-    map.setView(center);
-    return null;
+  const map = useMap();
+  map.setView(center);
+  return null;
 };
 
 interface LocationState {
-    latitude: number | null;
-    longitude: number | null;
+  latitude: number | null;
+  longitude: number | null;
 }
 
-export const MapPanel: React.FC = () => {
+const MapMarkers = (props: any) => {
+  const mapState = useMap();
+  const [zoomed, setZoomed] = React.useState(0);
 
+  mapState.on("zoomend", () => {
+    setZoomed(zoomed + 1);
+  });
 
-    const mooseIconMale = new Icon({
-        iconUrl: 'moose.png',
-        iconSize: [30, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    })
-    const mooseIconFemale = new Icon({
-        iconUrl: 'fmoose.png',
-        iconSize: [30, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    })
-    const mooseIconCalf = new Icon({
-        iconUrl: 'calf.png',
-        iconSize: [30, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    })
+  const markerState = useSelector(
+    (state: any) => state.MooseSightingsState.location
+  ) as LocationState;
+  const markerPosition: [number, number] = [
+    markerState.latitude ?? defaultLocation[0],
+    markerState.longitude ?? defaultLocation[1],
+  ];
+  const mooseArray = useSelector(
+    (state: any) => state.MooseSightingsState.mooseArray
+  ).slice(0, 5);
 
-    const markerState = useSelector((state: any) => state.MooseSightingsState.location) as LocationState;
-    const defaultLocation: [number, number] = [48.4284, -123.3656];
+  const getOffsetLocation = (
+    index: number,
+    baseLocation: [number, number]
+  ): [number, number] => {
+    const ogLatLng = new L.LatLng(baseLocation[0], baseLocation[1]);
+    const ogXY = mapState.latLngToContainerPoint(ogLatLng);
+    const offsetDistance = 10;
+    const newXY = ogXY.add([offsetDistance * index, 0]);
+    const offsetPoint = mapState.containerPointToLatLng(newXY);
 
-    const markerPosition: [number, number] = [
-        markerState.latitude ?? defaultLocation[0],
-        markerState.longitude ?? defaultLocation[1]
-    ];
+    return [offsetPoint.lat, offsetPoint.lng];
+  };
 
-    const mooseArray = useSelector((state: any) => state.MooseSightingsState.mooseArray).slice(0, 5);
-
-    const getOffsetLocation = (index: number, baseLocation: [number, number]): [number, number] => {
-        const offsetDistance = 0.002;
-        switch (index) {
-            case 1: // Behind (Bottom)
-                return [baseLocation[0] - 0.0015, baseLocation[1] + 0.0041];
-            case 2: // Behind (Top)
-                return [baseLocation[0] + 0.001, baseLocation[1] + 0.0041];
-            case 3: // Above
-                return [baseLocation[0] + offsetDistance, baseLocation[1]];
-            case 4: // Below
-                return [baseLocation[0] - offsetDistance, baseLocation[1]];
-            default: // First moose (no offset)
-                return baseLocation;
-        }
-    };
-
-    const getMooseIcon = (moose: any) => {
-        if (moose.age === 'Calf' && moose.age !== null) {
-            return mooseIconCalf;
-        }
-        if (moose.gender === 'male') {
-            return mooseIconMale;
-        }
-        if (moose.gender === 'female') {
-            return mooseIconFemale;
-        }
-        else {
-            return mooseIconMale;
-        }
+  const getMooseIcon = (moose: any) => {
+    if (moose.age === "Calf" && moose.age !== null) {
+      return mooseIconCalf;
     }
+    if (moose.gender === "male") {
+      return mooseIconMale;
+    }
+    if (moose.gender === "female") {
+      return mooseIconFemale;
+    } else {
+      return mooseIconMale;
+    }
+  };
 
-    return (
-        <div className="MapPanel">
-            <MapContainer className="MapContainer" center={defaultLocation} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {mooseArray.map((moose: any, index: number) => {
-                    const position = getOffsetLocation(index, markerPosition);
-                    const mooseIcon = getMooseIcon(moose);
-                    return <Marker key={index} position={position} icon={mooseIcon} />;
-                })}
-                <ChangeView center={markerPosition} />
-            </MapContainer>
-        </div>
-    );
+
+  const Meese = (props: any) => (
+    <>
+      {mooseArray.map((moose: any, index: number) => {
+        const position = getOffsetLocation(index, markerPosition);
+        const mooseIcon = getMooseIcon(moose);
+        return <Marker key={index} position={position} icon={mooseIcon} />;
+      })}
+    </>
+  );
+
+  return <Meese key={zoomed} />;
+};
+
+const defaultLocation: [number, number] = [48.4284, -123.3656];
+export const MapPanel: React.FC = () => {
+  const markerState = useSelector(
+    (state: any) => state.MooseSightingsState.location
+  ) as LocationState;
+  const markerPosition: [number, number] = [
+    markerState.latitude ?? defaultLocation[0],
+    markerState.longitude ?? defaultLocation[1],
+  ];
+
+  return (
+    <div className="MapPanel">
+      <MapContainer
+        className="MapContainer"
+        center={defaultLocation}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MapMarkers />
+        <ChangeView center={markerPosition} />
+      </MapContainer>
+    </div>
+  );
 };
