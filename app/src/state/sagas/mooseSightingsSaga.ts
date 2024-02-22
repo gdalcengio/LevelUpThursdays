8cca70df0a76dc1c0255482b02adbe834d1fca07
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2024-02-22 13:54:18 -0800

    fix ghost conflicts for julian

--- app/src/state/sagas/mooseSightingsSaga.ts
+++ app/src/state/sagas/mooseSightingsSaga.ts
@@ -1,13 +1,6 @@
 import { channel } from "redux-saga";
 import { useDispatch } from "react-redux";
-import {
-  all,
-  put,
-  call,
-  take,
-  takeEvery,
-  select
-} from "redux-saga/effects";
+import { all, put, call, take, takeEvery, select } from "redux-saga/effects";
 import {
   ACTIVITY_LOCATION_SET,
   USER_SAVE_SIGHTINGS,
@@ -23,7 +16,6 @@ import {
 function* handle_USER_CLICK_RECORD_MOOSE(action: any) {
   yield put({ type: ACTIVITY_LOCATION_SET, payload: location });
 }
-
 
 function* getGeoLocation(action: any) {
   const coordChannel = channel();
@@ -51,8 +43,6 @@ function* getGeoLocation(action: any) {
 
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-
-
   while (true) {
     const action: any = yield take(coordChannel);
     yield put(action);
@@ -60,17 +50,17 @@ function* getGeoLocation(action: any) {
   }
 }
 
-
-
 function* write_sightings_to_disk(action: any): Generator<any> {
-  const sightings: any = yield select((state: any) => state.MooseSightingsState.allSightings);
+  const sightings: any = yield select(
+    (state: any) => state.MooseSightingsState.allSightings
+  );
   localStorage.setItem("Sightings", JSON.stringify(sightings));
 }
 
-
 function* handle_USER_SAVE_SIGHTINGS(action: any) {
-
-  const mooseSightings: any = yield select((state: any) => state.MooseSightingsState);
+  const mooseSightings: any = yield select(
+    (state: any) => state.MooseSightingsState
+  );
 
   const mooseArray = mooseSightings.mooseArray;
   const mooseLocation = mooseSightings.location;
@@ -78,77 +68,71 @@ function* handle_USER_SAVE_SIGHTINGS(action: any) {
   let errors = [];
 
   if (mooseArray.length < 1) {
-    errors.push('Moose array cannot be empty.');
+    errors.push("Moose array cannot be empty.");
   }
   if (mooseLocation === undefined) {
-    errors.push('Moose location cannot be empty.');
+    errors.push("Moose location cannot be empty.");
   }
 
   if (errors.length) {
     yield put({ type: USER_SAVE_SIGHTINGS_FAIL, payload: { errors: errors } });
-  }
-  else {
+  } else {
     yield put({ type: USER_SAVE_SIGHTINGS_SUCCESS });
   }
-
 }
 
-<<<<<<< HEAD
 function* handle_USER_SAVE_SIGHTINGS_SUCCESS(action: any) {
   yield put({ type: WRITE_SIGHTINGS_TO_DISK });
 }
 
-=======
 function prepareSightingsForApi(sightings: any) {
-  return sightings.map(sighting => {
+  return sightings.map((sighting) => {
     return {
       id: sighting.id,
       dateOfSighting: sighting.dateOfSighting,
       status: "Synced",
       syncDate: Date.now(),
       location: [sighting.location.latitude, sighting.location.longitude],
-      mooseArray: sighting.mooseArray.map(moose => ({
+      mooseArray: sighting.mooseArray.map((moose) => ({
         id: moose.id,
         age: moose.age,
-        gender: moose.gender || "unknown"
-      }))
+        gender: moose.gender || "unknown",
+      })),
     };
   });
 }
->>>>>>> 9b1225a (Cleanup/Improvements)
 
 function fetchSightings(validatedSightings: any) {
-  return fetch('http://localhost:7080/recordSightings', {
-    method: 'POST',
+  return fetch("http://localhost:7080/recordSightings", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({ sightings: validatedSightings }),
-  })
-    .then(response => {
-      if (!response.ok) {
-        return response.json().then(errorResponse => Promise.reject(new Error(errorResponse)));
-      }
-      return response.json();
-    });
+  }).then((response) => {
+    if (!response.ok) {
+      return response
+        .json()
+        .then((errorResponse) => Promise.reject(new Error(errorResponse)));
+    }
+    return response.json();
+  });
 }
 
 function* handle_SYNC_SIGHTINGS_TO_DB(action: any) {
   try {
-    const storedSightings = yield select((state) => state.MooseSightingsState.allSightings);
+    const storedSightings = yield select(
+      (state) => state.MooseSightingsState.allSightings
+    );
     const validatedSightings = prepareSightingsForApi(storedSightings);
 
     const data = yield call(fetchSightings, validatedSightings);
 
     yield put({ type: SIGHTING_SYNC_SUCCESSFUL, payload: { data: data } });
-    console.log('Sightings synced successfully:', data);
+    console.log("Sightings synced successfully:", data);
   } catch (error) {
     console.log(error);
   }
-}
-
-function* handle_USER_SAVE_SIGHTINGS_SUCCESS(action: any) {
-  yield put({ type: WRITE_SIGHTINGS_TO_DISK });
 }
 
 function* mooseSightingSaga() {
@@ -157,15 +141,15 @@ function* mooseSightingSaga() {
       takeEvery(GET_GEOLOCATION, getGeoLocation),
       takeEvery(USER_SAVE_SIGHTINGS, handle_USER_SAVE_SIGHTINGS),
       takeEvery(WRITE_SIGHTINGS_TO_DISK, write_sightings_to_disk),
-      takeEvery(USER_SAVE_SIGHTINGS_SUCCESS, handle_USER_SAVE_SIGHTINGS_SUCCESS),
+      takeEvery(
+        USER_SAVE_SIGHTINGS_SUCCESS,
+        handle_USER_SAVE_SIGHTINGS_SUCCESS
+      ),
       takeEvery(SYNC_SIGHTINGS_TO_DB, handle_SYNC_SIGHTINGS_TO_DB),
       takeEvery(SIGHTING_SYNC_SUCCESSFUL, write_sightings_to_disk),
-      takeEvery(USER_SAVE_SIGHTINGS_SUCCESS, handle_USER_SAVE_SIGHTINGS_SUCCESS)
     ]);
-
-  }
-  catch (e) {
-    console.log(e)
+  } catch (e) {
+    console.log(e);
   }
 }
 
