41d704727c2dde235b2e8f5de51d1f3fabc47e1b
Luna Morris <127158867+lunamoonmoon@users.noreply.github.com>
2024-03-07 11:15:56 -0800

    Revert "Fix/route indication"

--- app/src/UI/Header.tsx
+++ app/src/UI/Header.tsx
@@ -1,9 +1,8 @@
 import "./Header.css"
-import { useNavigate, useLocation, NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Header = (props: any) => {
   const navigate = useNavigate();
-  const location = useLocation();
 
   const goHome = () => {
     navigate('/About');
@@ -17,6 +16,10 @@ export const Header = (props: any) => {
     navigate('/');
   }
 
+  const goToSightings = () => {
+    navigate("/Sightings");
+  }
+
     return(
       <header className="headerMain">
         <div className="headerWrapper">
@@ -27,20 +30,14 @@ export const Header = (props: any) => {
                 <p className="headerText">Moose Tracker</p>
             </div>
             <div className="headerButtonContainer">
-              <NavLink className={`headerButton ${location.pathname === "/" ? 'header-selected ' : ''}`} to="/">
+              <button className="headerButton" onClick={goToForm}>
                 Add a Moose Sighting
-              </NavLink>
-              <NavLink className={`headerButton ${location.pathname === "/Sightings" ? 'header-selected ' : ''}`} to="/Sightings">
-                Sightings
-              </NavLink>
-              <NavLink className={`headerButton ${location.pathname === "/About" ? 'header-selected ' : ''}`} to="/About">
               </button>
               <button className="headerButton" onClick={goToRegs}>
                 Regulations
               </button>
               <button className="headerButton" onClick={goHome}>
                 About
-              </NavLink>
               </button>
               <button className="headerButton" onClick={goToSightings}>
                 Sightings
