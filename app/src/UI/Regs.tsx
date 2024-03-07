1388c93326328c2b35b189703ba01edc3d4b65b6
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2024-03-07 10:12:33 -0800

    Split pdf and style regs page

--- app/src/UI/Regs.tsx
+++ app/src/UI/Regs.tsx
@@ -7,7 +7,64 @@ export const Regs = (props: any) => {
   ref.current += 1;
   console.log("%Regs render:" + ref.current.toString(), "color: yellow");
 
-  return <a href='hunting-trapping-synopsis.pdf'>
-    <div className="PDFPanel"></div>
-  </a>
-}
+  const contentsMap = [
+    {
+      name: 'Region 1: Vancouver Island',
+      link: 'region1_vancouver_island.pdf'
+    },
+    {
+      name: 'Region 2: Lower Mainland',
+      link: 'region2_lower_mainland.pdf'
+    },
+    {
+      name: 'Region 3: Thompson',
+      link: 'region3_Thompson.pdf'
+    },
+    {
+      name: 'Region 4: Kootenay',
+      link: 'region4_kootenay.pdf'
+    },
+    {
+      name: 'Region 5: Cariboo',
+      link: 'region5_cariboo.pdf'
+    },
+    {
+      name: 'Region 6: Skeena',
+      link: 'region6_skeena.pdf'
+    },
+    {
+      name: 'Region 7a: Omineca',
+      link: 'region7a_omineca.pdf'
+    },
+    {
+      name: 'Region 7b: Peace',
+      link: 'region7b_peace.pdf'
+    },
+    {
+      name: 'Region 8: Okanagan',
+      link: 'region8_okanagan.pdf'
+    },
+  ]
+
+  return (
+    <div className="regs-component">
+      <div className="table-of-contents">
+        <h1>Regulations</h1>
+        <a href='hunting-trapping-synopsis.pdf' className="link">
+          <div className="pdf">Full Hunting and Trapping Regulations Synopsis 2022-2024</div>
+        </a>
+        <h2 className="title">
+          Contents By Region
+        </h2>
+        {contentsMap.map((content) => {
+        return (
+          <a href={content.link} className="link">
+            <div>
+              {content.name}
+            </div>
+          </a>
+          )
+        })}
+      </div>
+    </div>
+)}
